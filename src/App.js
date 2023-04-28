fd2c42ca250d3e6c34a559308cb4cba324abe7a6
Yash Malik <malikyash67@gmail.com>
2023-04-28 10:19:33 +0530

    fetch

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import MyClass from './Class';
 import Form  from './Form';
 import Form2 from './Form2';
 import Hoc from './Hoc';
+import ApiCall from './ApiCall';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
      <Form />
      <Form2 />
      <Hoc/>
+     <ApiCall />
     </div>
   );
 }
---
+++ src/PostApi.jsx
@@ -0,0 +1,7 @@
+ export default function PostAPi(){
+    return(
+        <>
+        <h1>Hello from Post Api</h1>
+        </>
+    )
+}
